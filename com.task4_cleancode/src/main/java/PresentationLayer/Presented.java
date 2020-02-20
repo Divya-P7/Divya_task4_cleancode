36382ca7be683d09b4d09aad1401d135c15f0abe
Divya-P7 <60293358+Divya-P7@users.noreply.github.com>
2020-02-20 19:27:16 +0530

    Add files via upload

---
+++ com.task4_cleancode/src/main/java/PresentationLayer/Presented.java
@@ -0,0 +1,6 @@
+package PresentationLayer;
+
+public interface Presented {
+int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+}
